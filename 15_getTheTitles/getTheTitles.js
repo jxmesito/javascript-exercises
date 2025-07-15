0844fbe74fb224dc17e58e64bd71d9c1e2887682
jxmesito <jamesmhernandez732@gmail.com>
2025-07-15 11:59:47 -0500

    added exercise

--- 15_getTheTitles/getTheTitles.js
+++ 15_getTheTitles/getTheTitles.js
@@ -1,5 +1,22 @@
-const getTheTitles = function() {
+const getTheTitles = function(books) {
+// const books = [
+//   {
+//     title: 'Book',
+//     author: 'Name'
+//   },
+//   {
+//     title: 'Book2',
+//     author: 'Name2'
+//   }
+// ]
 
+// getTheTitles(books) // ['Book','Book2']
+
+// input - arr of dictionaries
+// output - arr of book titles
+    let titles = books.map(book => book.title);
+
+    return titles;
 };
 
 // Do not edit below this line
